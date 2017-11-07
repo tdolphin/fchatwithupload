1ba799db32b7767b85d070167b18f764b4027a68
Tamer Mohamed <tamer@MacBook-Pro.local>
2017-11-07 09:22:49 -0500

    simplified code

--- public/app.js
+++ public/app.js
@@ -1,4 +1,42 @@
 'use strict';
+
+function $id(id) { return document.getElementById(id); }
+
+function setoptions(pnode, options) {
+    if (options) {
+        for (var key in options) {
+            if (options.hasOwnProperty(key)) {
+                pnode.setAttribute(key, options[key]);
+            }
+        }
+    }
+}
+
+var mdom = {
+
+    clearaDiv: function(divId) {
+        var box = $id(divId);
+        var newBox = box.cloneNode(false);
+        box.parentNode.replaceChild(newBox, box);
+        box = newBox;
+    },
+
+    addhref: function(pNode, stext, href, options) {
+        var aref = document.createElement('a');
+        aref.setAttribute('href', href);
+        aref.innerHTML = stext;
+        setoptions(aref, options);
+        pNode.appendChild(aref);
+        return aref;
+    },
+
+    addbreak: function(pNode, n) {
+        n = n || 0;
+        for (var i = 0; i < n; i++)
+            pNode.appendChild(document.createElement('br'));
+    }
+};
+
 // Establish a Socket.io connection
 const socket = io();
 // Initialize our Feathers client application through Socket.io
@@ -9,7 +47,7 @@ client.configure(feathers.socketio(socket));
 client.configure(feathers.hooks());
 // Use localStorage to store our login token
 client.configure(feathers.authentication({
-  storage: window.localStorage
+    storage: window.localStorage
 }));
 
 // Login screen
@@ -94,75 +132,80 @@ const chatHTML = `<main class="flex flex-column">
 </main>`;
 
 function loginclick(ev) {
-  switch(ev.target.id) {
-  case 'signup': {
-    const user = getCredentials();
-    // For signup, create a new user and then log them in
-    client.service('users').create(user)
-      .then(() => login(user));
+    switch (ev.target.id) {
+        case 'signup':
+            {
+                const user = getCredentials();
+                // For signup, create a new user and then log them in
+                client.service('users').create(user)
+                .then(() => login(user));
 
-    break;
-  }
-  case 'login': {
-    const user = getCredentials();
-    login(user);
+                break;
+            }
+        case 'login':
+            {
+                const user = getCredentials();
+                login(user);
 
-    break;
-  }
-  case 'logout': {
-    client.logout().then(() => {
-      document.getElementById('app').innerHTML = loginHTML;
-    });
+                break;
+            }
+        case 'logout':
+            {
+                client.logout().then(() => {
+                    document.getElementById('app').innerHTML = loginHTML;
+                });
 
-    break;
-  }
-  case 'uploadButton': {
-    uploadAttachment();
-    break;
-  }
-  case 'attchmntButton': {
-    document.getElementById('attachmentmenu').classList.toggle('show');
-    break;
-  }
+                break;
+            }
+        case 'uploadButton':
+            {
+                uploadAttachment();
+                break;
+            }
+        case 'attchmntButton':
+            {
+                document.getElementById('attachmentmenu').classList.toggle('show');
+                break;
+            }
 
-  }
+    }
 }
 // Show the login page
-function showLogin(error ) {
-  error = error || {};
-  if(document.querySelectorAll('.login').length) {
-    document.querySelector('.heading').insertAdjacentHTML('beforeend', `<p>There was an error: ${error.message}</p>`);
-  } else {
-    document.getElementById('app').innerHTML = loginHTML;
-  }
+function showLogin(error) {
+    error = error || {};
+    if (document.querySelectorAll('.login').length) {
+        document.querySelector('.heading').insertAdjacentHTML('beforeend', `<p>There was an error: ${error.message}</p>`);
+    } else {
+        document.getElementById('app').innerHTML = loginHTML;
+    }
 }
 
 // Add a new user to the list
 function addUser(user) {
-  const userList = document.querySelector('.user-list');
+    const userList = document.querySelector('.user-list');
 
-  if(userList) {
-    // Add the user to the list
-    userList.insertAdjacentHTML('beforeend', `<li>
+    if (userList) {
+        // Add the user to the list
+        userList.insertAdjacentHTML('beforeend', `<li>
       <a class="block relative" href="#">
         <img src="${user.avatar}" alt="" class="avatar">
         <span class="absolute username">${user.email}</span>
       </a>
     </li>`);
 
-    // Update the number of users
-    document.querySelector('.online-count').innerHTML = document.querySelectorAll('.user-list li').length;
-  }
+        // Update the number of users
+        document.querySelector('.online-count').innerHTML = document.querySelectorAll('.user-list li').length;
+    }
 }
 
 // Renders a new message and finds the user that belongs to the message
 function addMessage(message) {
-  // Find the user belonging to this message or use the anonymous user if not found
-  const sender = message.user || {};
-  const chat = document.querySelector('.chat');
+    // Find the user belonging to this message or use the anonymous user if not found
+    const sender = message.user || {};
+    const chat = document.querySelector('.chat');
 
-  if(chat) {
-    chat.insertAdjacentHTML( 'beforeend', `<div class="message flex flex-row">
+    if (chat) {
+        chat.insertAdjacentHTML('beforeend', `<div class="message flex flex-row">
       <img src="${sender.avatar}" alt="${sender.email}" class="avatar">
       <div class="message-wrapper">
         <p class="message-header">
@@ -173,168 +216,160 @@ function addMessage(message) {
       </div>
     </div>`);
 
-    chat.scrollTop = chat.scrollHeight - chat.clientHeight;
-  }
+        chat.scrollTop = chat.scrollHeight - chat.clientHeight;
+    }
 }
 
 // Shows the chat page
 function showChat() {
-  document.getElementById('app').innerHTML = chatHTML;
-  // Find the latest 10 messages. They will come with the newest first
-  // which is why we have to reverse before adding them
-  client.service('messages').find({
-    query: {
-      $sort: { createdAt: -1 },
-      $limit: 25
-    }
-  }).then(page => page.data.reverse().forEach(addMessage));
+    document.getElementById('app').innerHTML = chatHTML;
+    // Find the latest 10 messages. They will come with the newest first
+    // which is why we have to reverse before adding them
+    client.service('messages').find({
+        query: {
+            $sort: { createdAt: -1 },
+            $limit: 25
+        }
+    }).then(page => page.data.reverse().forEach(addMessage));
 
-  client.service('uploads').find().then(function(uploads) {
-    mdom.clearaDiv('currentAttachments');
-    uploads.data.forEach(addAttachment);
-  });
+    client.service('uploads').find().then(function(uploads) {
+        mdom.clearaDiv('currentAttachments');
+        uploads.data.forEach(addAttachment);
+    });
 
-  // Find all users
-  client.service('users').find().then(page => {
-    const users = page.data;
+    // Find all users
+    client.service('users').find().then(page => {
+        const users = page.data;
 
-    // Add every user to the list
-    users.forEach(addUser);
-  });
+        // Add every user to the list
+        users.forEach(addUser);
+    });
 }
 
 // Retrieve email/password object from the login/signup page
 function getCredentials() {
-  const user = {
-    email: document.querySelector('[name="email"]').value,
-    password: document.querySelector('[name="password"]').value
-  };
+    const user = {
+        email: document.querySelector('[name="email"]').value,
+        password: document.querySelector('[name="password"]').value
+    };
 
-  return user;
+    return user;
 }
 
 // Log in either using the given email/password or the token from storage
 function login(credentials) {
-  const payload = credentials ?
-    Object.assign({ strategy: 'local' }, credentials) : {};
+    const payload = credentials ?
+        Object.assign({ strategy: 'local' }, credentials) : {};
 
-  return client.authenticate(payload)
-    .then(showChat)
-    .catch(showLogin);
+    return client.authenticate(payload)
+        .then(showChat)
+        .catch(showLogin);
 }
 
 function attclick(e) {
-  e.preventDefault();
-  var fileId = e.target.getAttribute('data');  
-  var aref = mdom.addhref(window.document.body,'','/download/'+fileId+'?t='+window.localStorage['feathers-jwt']);
-  aref.click();
-  window.document.body.removeChild(aref);
+    e.preventDefault();
+    var fileId = e.target.getAttribute('data');
+    var aref = mdom.addhref(window.document.body, '', '/download/' + fileId + '?t=' + window.localStorage['feathers-jwt']);
+    aref.click();
+    window.document.body.removeChild(aref);
 
-  // utils.ajaxGet('/download/'+fileId,
-  //   function(r) {
-  //     return false;     
-  //   }
-  
-  // ,null,{'Authorization':'Bearer '+window.localStorage['feathers-jwt']});
-  return false; 
+    // utils.ajaxGet('/download/'+fileId,
+    //   function(r) {
+    //     return false;     
+    //   }
+
+    // ,null,{'Authorization':'Bearer '+window.localStorage['feathers-jwt']});
+    return false;
 }
+
 function addAttachment(attachment) {
-  //console.log(attachment);
-  var aattch = attachment;
-  var attDiv = $id('currentAttachments');
-  //var firstdiv = $id('firstuserMdivider');
-  var fileSize;
-  if (aattch.filesize > 1024 * 1024) {
-    fileSize = (Math.round(aattch.filesize * 100 / (1024 * 1024)) / 100).toString() + 'MB';
-  } else {
-    fileSize = (Math.round(aattch.filesize * 100 / 1024) / 100).toString() + 'KB';
-  }
-  //var butcb = (function(attid) {
-  //    return function() {	
-  //      utils.ajaxGet("delattachment.php?attID=" + attid,
-  //        function(response) {
-  //          ShowMessage(response);
-  //          getAttachments();
-  //        }
-  //      );
-  //    }
-  //  })(aattch._id);
-  var aref = mdom.addhref(attDiv,aattch.filename + ' (' + fileSize + ')','#',{'style':'display:inline-block', 'class':'attachmentlink','data':aattch._id});
-  aref.addEventListener('click',attclick);
-  //mdom.addbutton(attDiv, "del", "del_"+aattch.att_pk, butcb, {"style":"float:right"});
-  mdom.addbreak(attDiv,1);
-  var atcount = document.querySelectorAll('#currentAttachments a').length;
-  $id('attchmntButton').innerHTML = atcount + ' Attachment' +((atcount>1)?'s':'');
+    //console.log(attachment);
+    var aattch = attachment;
+    var attDiv = $id('currentAttachments');
+    //var firstdiv = $id('firstuserMdivider');
+    var fileSize;
+    if (aattch.filesize > 1024 * 1024) {
+        fileSize = (Math.round(aattch.filesize * 100 / (1024 * 1024)) / 100).toString() + 'MB';
+    } else {
+        fileSize = (Math.round(aattch.filesize * 100 / 1024) / 100).toString() + 'KB';
+    }
+
+    var aref = mdom.addhref(attDiv, aattch.filename + ' (' + fileSize + ')', '#', { 'style': 'display:inline-block', 'class': 'attachmentlink', 'data': aattch._id });
+    aref.addEventListener('click', attclick);
+    //mdom.addbutton(attDiv, "del", "del_"+aattch.att_pk, butcb, {"style":"float:right"});
+    mdom.addbreak(attDiv, 1);
+    var atcount = document.querySelectorAll('#currentAttachments a').length;
+    $id('attchmntButton').innerHTML = atcount + ' Attachment' + ((atcount > 1) ? 's' : '');
 }
 
 function uploadAttachment() {
-  if(!$id('fileselect').files[0]) return;
-	
-  var acceptedTypes = {
-    'image/png': true,
-    'image/jpeg': true,
-    'application/excel': true,
-	  'application/pdf':true
-  };
-	
-  var file = $id('fileselect').files[0];
-  if(acceptedTypes[file.type] !== true) {
-    console.log('file type not supported');
-    return;
-  }
-  if(file.size > 5242880) {
-    console.log('file too large. 5MB limit.');
-    return;
-  }
-	
-  
-  $id('uploadButton').style.display = 'none';
-  $id('uploadprgrs').value = 0;
-  $id('uploadprgrs').style.display = 'inline-block';
-	
-  var fd = new FormData();
-  
-  fd.append('attchfile', file);
-	
-  var xhr = new XMLHttpRequest();
-	
-  xhr.upload.onprogress = function(e) {
-    if (e.lengthComputable) {
-      var complete = (e.loaded / e.total * 100 | 0);
-      $id('uploadprgrs').value = $id('uploadprgrs').innerHTML = complete;
+    if (!$id('fileselect').files[0]) return;
+
+    var acceptedTypes = {
+        'image/png': true,
+        'image/jpeg': true,
+        'application/excel': true,
+        'application/pdf': true
+    };
+
+    var file = $id('fileselect').files[0];
+    if (acceptedTypes[file.type] !== true) {
+        console.log('file type not supported');
+        return;
     }
-  };
-	
-  xhr.onload =  function() {
-    $id('uploadprgrs').style.display = 'none';
-    $id('uploadButton').style.display = 'inline-block';
-    $id('fileselect').value = '';
-    if (xhr.status === 201) {
-      console.log('File Attached');
-    } else {
-      console.log('Error '+ xhr.status  +': File not Attached');
+    if (file.size > 5242880) {
+        console.log('file too large. 5MB limit.');
+        return;
     }
-  };
-	
-  xhr.open('POST', '/uploads');
-  xhr.setRequestHeader('Authorization','Bearer '+window.localStorage['feathers-jwt']);
-  xhr.send(fd);
+
+
+    $id('uploadButton').style.display = 'none';
+    $id('uploadprgrs').value = 0;
+    $id('uploadprgrs').style.display = 'inline-block';
+
+    var fd = new FormData();
+
+    fd.append('attchfile', file);
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.upload.onprogress = function(e) {
+        if (e.lengthComputable) {
+            var complete = (e.loaded / e.total * 100 | 0);
+            $id('uploadprgrs').value = $id('uploadprgrs').innerHTML = complete;
+        }
+    };
+
+    xhr.onload = function() {
+        $id('uploadprgrs').style.display = 'none';
+        $id('uploadButton').style.display = 'inline-block';
+        $id('fileselect').value = '';
+        if (xhr.status === 201) {
+            console.log('File Attached');
+        } else {
+            console.log('Error ' + xhr.status + ': File not Attached');
+        }
+    };
+
+    xhr.open('POST', '/uploads');
+    xhr.setRequestHeader('Authorization', 'Bearer ' + window.localStorage['feathers-jwt']);
+    xhr.send(fd);
 }
 
 document.addEventListener('click', loginclick);
 document.addEventListener('submit', function(ev) {
-  if(ev.target.id === 'send-message') {
-    // This is the message text input field
-    const input = document.querySelector('[name="text"]');
+    if (ev.target.id === 'send-message') {
+        // This is the message text input field
+        const input = document.querySelector('[name="text"]');
 
-    // Create a new message and then clear the input field
-    client.service('messages').create({
-      text: input.value
-    }).then(() => {
-      input.value = '';
-    });
-    ev.preventDefault();
-  }
+        // Create a new message and then clear the input field
+        client.service('messages').create({
+            text: input.value
+        }).then(() => {
+            input.value = '';
+        });
+        ev.preventDefault();
+    }
 });
 
 // Listen to created events and add the new message in real-time
